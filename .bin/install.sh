1eb4c4a24ec05529b5c35401578181010e765b31
Jasper Keuper <jkeuper@gmail.com>
2019-10-29 00:02:08 +0100

    added install script

---
+++ .bin/install.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+git clone --bare git@github.com:jkeuper/dotfiles.git $HOME/.cfg
+
+function cfgit {
+ /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+echo "alias cfgit='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> $HOME/.bash_aliases
+
+cfgit config --local status.showUntrackedFiles no
+cfgit push --set-upstream origin master
+
+cfgit checkout &> /dev/null
+if ! [ $? = 0 ]; then
+  mkdir -p .cfg-backup
+  cfgit checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .cfg-backup/{}
+  echo "Backing up pre-existing dot files to ~/.cfg-backup/";
+  cfgit checkout
+  # the .bash_aliases is overwritten here with the cfgit alias already present
+fi;
+
